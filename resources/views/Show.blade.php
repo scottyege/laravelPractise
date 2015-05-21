7537ac8c969bd19b2ece993c4ab4810e25b69bda
Scott_Chiu <scott_chiu@xpec.com>
2015-05-21 16:29:56 +0800

    add play/stop button to the show page, play one by one

--- resources/views/Show.blade.php
+++ resources/views/Show.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>SimAuto</title>
+	<title>History</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -46,16 +46,10 @@
 		left: 160px;
 	}
 
-	#startBtn, #stopBtn {
+	#playBtn, #stopBtn {
 		position: absolute;
 		top: 100px;
 		left: 100px;
-	}
-
-	#saveBtn {
-		position: absolute;
-		top: 100px;
-		left: 200px;
 	}
 
 	#information {
@@ -100,19 +94,11 @@
 
 </head>
 <body>
-	<?php
-	    $encrypter = app('Illuminate\Encryption\Encrypter');
-	    $encrypted_token = $encrypter->encrypt(csrf_token());
-	?>
-	<input id="token" type="hidden" value="{{$encrypted_token}}">
-
-	
 
 	<div id='content'>
-		<a id='startBtn' class="btn btn-default btn-lg">Start</a>
+		<a id='playBtn' class="btn btn-default btn-lg">Play</a>
 		<a id='stopBtn' class="btn btn-default btn-lg">Stop</a>
 
-		<a id='saveBtn' class="btn btn-default btn-lg">Save</a>
 		<table id='t1'>
 			@for($i = 0; $i < ($n - 1); $i++)
 			<tr>
@@ -144,43 +130,45 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	<script>
 
-	$(document).ready(function() {
-		
-		var record = '<?php echo $record; ?>';
-		var obj = JSON.parse(record);
-		console.log(obj);
-		for(var i = 0; i < obj.length; i++)
-		{
-			if(obj[i].valid)
+	var historyIdx = 0;
+	var maxLenght= 0;
+	var historyInterval;
+	var obj = {};
+
+	function playHistory(interval)
+	{
+		historyInterval = setInterval(function(){
+
+			if(obj[historyIdx].valid)
 			{
-				var target = $('#' + obj[i].step.id);
+				var target = $('#' + obj[historyIdx].step.id);
 				target.css({
-						'background-color': obj[i].step.turn,
-						'color': (obj[i].step.turn == 'black' ? 'white' : 'black')
+						'background-color': obj[historyIdx].step.turn,
+						'color': (obj[historyIdx].step.turn == 'black' ? 'white' : 'black')
 				});
-				target.text(obj[i].step.step);
+				target.text(obj[historyIdx].step.step);
 
 				var informBlock = $('#information');
 				//informBlock.append('<p>' + obj.step.step + ' : ' + obj.step.turn + ' at ' + obj.step.id + '</p>');
 				var setDiv = $(document.createElement('div'));
 				var p = $(document.createElement('p'));
-				p.text(obj[i].step.step + ' : ' + obj[i].step.turn + ' at ' + obj[i].step.id);
+				p.text(obj[historyIdx].step.step + ' : ' + obj[historyIdx].step.turn + ' at ' + obj[historyIdx].step.id);
 				
 				//informBlock.append(p);
 				setDiv.append(p);
 
 				//kill
 				var ul = $(document.createElement('ul'));
-				for(var j = 0; j < obj[i].kill.length; j++)
+				for(var j = 0; j < obj[historyIdx].kill.length; j++)
 				{
-					var victim = $('#' + obj[i].kill[j]);
+					var victim = $('#' + obj[historyIdx].kill[j]);
 					victim.css({
 						'background-color': '',
 					});
 					victim.text('');
 
 					var li = $(document.createElement('li'));
-					li.text((obj[i].step.turn == 'black' ? 'white' : 'black') + ' die at ' + obj[i].kill[j]);
+					li.text((obj[historyIdx].step.turn == 'black' ? 'white' : 'black') + ' die at ' + obj[historyIdx].kill[j]);
 					ul.append(li);
 				}
 
@@ -193,13 +181,49 @@
 				//console.log('invalid move');
 				var setDiv = $(document.createElement('div'));
 				var p = $(document.createElement('p'));
-				p.text(obj[i].step.step + ' : ' + obj[i].step.turn + ' give up');
+				p.text(obj[historyIdx].step.step + ' : ' + obj[historyIdx].step.turn + ' give up');
 
 				setDiv.append(p);
 				$('#information').prepend(setDiv);
 			}
-		}
 
+			historyIdx++;
+			if(historyIdx >= maxLenght)
+			{
+				clearInterval(historyInterval);
+			}
+		}, interval);
+	}
+
+	$(document).ready(function() {
+		
+		var record = '<?php echo $record; ?>';
+		obj = JSON.parse(record);
+		maxLenght = obj.length;
+//		console.log(obj);
+
+		$('#stopBtn').hide();
+
+		$('#playBtn').click(function(){
+
+			if(historyIdx < maxLenght)
+			{
+				playHistory(1500);
+
+				$('#playBtn').hide();
+				$('#stopBtn').show();
+			}
+
+		});
+
+		$('#stopBtn').click(function(){
+
+			$('#playBtn').show();
+			$('#stopBtn').hide();
+			
+			clearInterval(historyInterval);
+
+		})
 	});
 
 	</script>
